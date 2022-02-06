6855993bcf72e3d337759992d2063b8556f94dd7
Nirodya Pussadeniya <en95142@sjp.ac.lk>
2022-02-06 19:49:29 +0530

    Initial commit

---
+++ lib/login_screen.dart
@@ -0,0 +1,202 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:login_signup/signup_screen.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:flutter/material.dart';
+
+//code for designing the UI of our text field where the user writes his email id or password
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+final _auth = FirebaseAuth.instance;
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool showSpinner = false;
+  bool wrongEmail = false;
+  bool wrongPassword = false;
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController _emailController = TextEditingController();
+    TextEditingController _passwordController = TextEditingController();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: ModalProgressHUD(
+          inAsyncCall: showSpinner,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                const Text(
+                  " Study Guider",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  margin: const EdgeInsets.all(4.0),
+                  padding: const EdgeInsets.all(20.0),
+                  decoration: BoxDecoration(
+                    // color: const Color(0xFF323F68),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        blurRadius: 7,
+                        spreadRadius: 1,
+                        offset: Offset(2, 4), // Shadow position
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const Text(
+                        "Login",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 44.0,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      const SizedBox(
+                        height: 44.0,
+                      ),
+                      TextField(
+                        controller: _emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                            hintText: "Email",
+                            prefixIcon: Icon(
+                              Icons.mail,
+                              color: Colors.black38,
+                            )),
+                      ),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      TextField(
+                        controller: _passwordController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                            hintText: "Password",
+                            prefixIcon: Icon(
+                              Icons.lock,
+                              color: Colors.black38,
+                            )),
+                      ),
+                      SizedBox(
+                        height: 16.0,
+                      ),
+                      Container(
+                        alignment: Alignment.centerRight,
+                        child: const Text(
+                          "Forgot password? ",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 16.0,
+                            // fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 12.0,
+                      ),
+                      RawMaterialButton(
+                          fillColor: Colors.amberAccent,
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 18.0),
+                          ),
+                          padding: const EdgeInsets.symmetric(vertical: 20.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                          onPressed: () async {
+                            setState(() {
+                              showSpinner = true;
+                            });
+                            try {
+                              setState(() {
+                                wrongEmail = false;
+                                wrongPassword = false;
+                              });
+                              final user =
+                                  await _auth.signInWithEmailAndPassword(
+                                email: _emailController.text,
+                                password: _passwordController.text,
+                              );
+                              if (user != null) {
+                                Navigator.pushNamed(context, 'home_screen');
+                                setState(() {
+                                  showSpinner = false;
+                                });
+                              } else {
+                                setState(() {});
+                              }
+                            } catch (e) {
+                              print(e);
+
+                              setState(() {
+                                showSpinner = false;
+                              });
+                            }
+                          }),
+                      SizedBox(
+                        height: 16.0,
+                      ),
+                      Container(
+                        alignment: Alignment.center,
+                        // color: Colors.blue[600],
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              "Don't have an account? ",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                textStyle: const TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              onPressed: () {
+                                Navigator.of(context).pushReplacement(
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            RegistrationScreen()));
+                              },
+                              child: const Text('Sign Up'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/signup_screen.dart
@@ -0,0 +1,216 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:login_signup/login_screen.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'rounded_button.dart';
+
+//code for designing the UI of our text field where the user writes his email id or password
+
+class RegistrationScreen extends StatefulWidget {
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
+  bool showSpinner = false;
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController _nameController = TextEditingController();
+    TextEditingController _emailController = TextEditingController();
+    TextEditingController _passwordController = TextEditingController();
+    TextEditingController _passwordConfirmationController =
+        TextEditingController();
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: ModalProgressHUD(
+          inAsyncCall: showSpinner,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                const Text(
+                  " Study Guider",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  // height: 500.0,
+                  // margin: const EdgeInsets.all(4.0),
+                  padding: const EdgeInsets.all(20.0),
+                  decoration: BoxDecoration(
+                    // color: const Color(0xFF323F68),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        blurRadius: 7,
+                        spreadRadius: 1,
+                        offset: Offset(2, 4), // Shadow position
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  // margin: 20.0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const Text(
+                        "Register",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 44.0,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      const SizedBox(
+                        height: 44.0,
+                      ),
+                      TextField(
+                        controller: _nameController,
+                        keyboardType: TextInputType.name,
+                        decoration: const InputDecoration(
+                            hintText: "Name",
+                            prefixIcon: Icon(
+                              Icons.account_box,
+                              color: Colors.black38,
+                            )),
+                      ),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      TextField(
+                        controller: _emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                            hintText: "Email",
+                            prefixIcon: Icon(
+                              Icons.mail,
+                              color: Colors.black38,
+                            )),
+                      ),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      TextField(
+                        controller: _passwordController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          hintText: "Password",
+                          prefixIcon: Icon(
+                            Icons.lock,
+                            color: Colors.black38,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      TextField(
+                        controller: _passwordConfirmationController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          hintText: "Confirm Password",
+                          prefixIcon: Icon(
+                            Icons.lock,
+                            color: Colors.black38,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 24.0,
+                      ),
+                      RawMaterialButton(
+                        fillColor: Colors.amberAccent,
+                        child: const Text(
+                          "Register",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 18.0),
+                        ),
+                        padding: const EdgeInsets.symmetric(vertical: 20.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        onPressed: () async {
+                          setState(() {
+                            showSpinner = true;
+                          });
+                          try {
+                            final newUser =
+                                await _auth.createUserWithEmailAndPassword(
+                              email: _emailController.text,
+                              password: _passwordController.text,
+                            );
+                            if (newUser != null) {
+                              Navigator.pushNamed(context, 'home_screen');
+                              setState(() {
+                                showSpinner = false;
+                              });
+                            }
+                          } catch (e) {
+                            print(e);
+                            setState(() {
+                              showSpinner = false;
+                            });
+                          }
+                        },
+                      ),
+                      SizedBox(
+                        height: 16.0,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 32.0,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  // color: Colors.blue[600],
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Already have an account? ",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          textStyle: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                  builder: (context) => LoginScreen()));
+                        },
+                        child: const Text('Login here'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
